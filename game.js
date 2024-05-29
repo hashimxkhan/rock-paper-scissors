5144652aaa4af22ef58c3d5935c830f01700e4a7
hashimxkhan <khan521@purdue.edu>
2024-05-29 15:05:09 +0500

    Added user interface added a user interface with buttons and winner announcements

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
+let round = 0;
 function getComputerChoice() {
   let value = Math.floor(Math.random() * 3);
   switch (value) {
@@ -11,27 +12,6 @@ function getComputerChoice() {
         return "scissors";
   }
 }
-function getHumanChoice() {
-    let answer =  prompt("Enter your answer");
-    answer = answer.toLowerCase().trim();
-    console.log(answer);
-    while (answer != "rock" && answer != "scissors" && answer != "paper") {
-    answer = prompt("Enter a valid answer");
-    answer = answer.toLowerCase().trim();
-    console.log(answer);
-    }
-    if (answer === "rock") {
-        return "rock";
-    }
-    if (answer === "paper") {
-        return "paper";
-    }
-    if (answer === "scissors") {
-        return "scissors";
-    }
-    return null;
-}
-function playGame() {
 function playRound(humanChoice, computerChoice) {
    if (humanChoice === "rock") {
     switch(computerChoice) {
@@ -42,13 +22,13 @@ function playRound(humanChoice, computerChoice) {
             humanWins(humanChoice, computerChoice);
             break;
         case "rock":
-            console.log("Its a tie!");
+            round_winner.textContent = "Its a tie..."
    } 
    }
    if (humanChoice === "paper") {
     switch(computerChoice) {
         case "paper":
-            console.log("Its a tie!");
+            round_winner.textContent = "Its a tie..."
             break;    
         case "scissors":
             computerWins(humanChoice, computerChoice);
@@ -63,39 +43,70 @@ function playRound(humanChoice, computerChoice) {
             humanWins(humanChoice,computerChoice);
             break;    
         case "scissors":
-            console.log("Its a tie!");
+            round_winner.textContent = "Its a tie..."
             break;
         case "rock":
             computerWins(humanChoice,computerChoice);
    } 
    }
+   human.textContent = "Your Score: " + humanScore
+   robot.textContent = "Robot's Score: " + computerScore
+
+   if (humanScore == 5) {
+     humanScore = 0
+     computerScore = 0
+     winner.textContent = "You win the game!"
+   } else if (computerScore == 5) {
+    humanScore = 0
+     computerScore = 0
+     winner.textContent = "You lose the game ;("
+   } else {
+    winner.textContent = ""
+   }
 }
 function humanWins(humanChoice, computerChoice) {
     humanScore++;
-    console.log("You win! " + humanChoice + " beats " + computerChoice)
+    round_winner.textContent = "You win this round!! " + humanChoice + " beats " + computerChoice
 }
 function computerWins(humanChoice,computerChoice) {
     computerScore++;
-    console.log("You lose! " + computerChoice + " beats " + humanChoice)
+    round_winner.textContent = "You lose! " + computerChoice + " beats " + humanChoice
 }
-const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
-console.log(playRound(humanSelection, computerSelection));
-console.log("Your score: " + humanScore);
-console.log("Computer's score: " + computerScore);
-}
-playGame();
-playGame();
-playGame();
-playGame();
-playGame();
-function result(humanScore, computerScore) {
-    if (humanScore > computerScore) {
-        console.log("You win!");
-    } else if (humanScore < computerScore) {
-        console.log("You lose :(");
-    } else {
-        console.log("its a tie...")
-    }
-}
-result(humanScore, computerScore);
+const section = document.querySelector(".section")
+const rock = document.createElement("button")
+const paper = document.createElement("button")
+const scissors = document.createElement("button")
+rock.textContent = "Rock"
+paper.textContent = "Paper"
+scissors.textContent = "Scissors"
+section.appendChild(rock)
+section.appendChild(paper)
+section.appendChild(scissors)
+section.style.paddingLeft = "555px"
+paper.style.margin ="18px"
+
+rock.addEventListener("click", function() {
+    playRound("rock", getComputerChoice())
+})
+
+paper.addEventListener("click", function() {
+    playRound("paper", getComputerChoice())
+})
+
+scissors.addEventListener("click", function() {
+    playRound("scissors", getComputerChoice())
+})
+
+const scores = document.querySelector(".scores")
+const human = document.createElement("p")
+const robot = document.createElement("p")
+const winner = document.createElement("p")
+const round_winner = document.createElement("p")
+human.textContent = "Your Score: " + humanScore
+robot.textContent = "Robot's Score: " + computerScore
+scores.appendChild(winner)
+scores.appendChild(round_winner)
+scores.appendChild(human)
+scores.appendChild(robot)
+
